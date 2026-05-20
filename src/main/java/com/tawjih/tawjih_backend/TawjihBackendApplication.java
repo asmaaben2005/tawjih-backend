ca591b74c2ec1c49aeee3d1d1cb4104efbd8812f
8Remma <157365993+asmaaben2005@users.noreply.github.com>
2026-05-20 09:56:10 +0100

    Fix: Final Clean Architecture with all files and packages

---
+++ src/main/java/com/tawjih/tawjih_backend/TawjihBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tawjih.tawjih_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TawjihBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TawjihBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tawjih/tawjih_backend/TawjihBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tawjih.tawjih_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TawjihBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
